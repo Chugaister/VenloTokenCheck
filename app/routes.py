b9209c5c03644c7abc58cb6f8ff8a25810bf7012
Chugaister <dmytroshvahyliak@gmail.com>
2023-01-25 16:28:42 +0200

    api added

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,27 @@
-from flask import render_template
+from flask import render_template, jsonify, request
+from core.scanner import verify, available_networks
 from app import app
 
 
-@app.route('/')
-@app.route('/index')
+@app.route("/")
+@app.route("/index")
 def index():
     return render_template("index.html")
+
+
+@app.route("/api/networks")
+def api_networks():
+    return jsonify({
+        "status": 1,
+        "response": available_networks
+    })
+
+
+@app.route("/api/verify")
+def api_verify():
+    network = request.args.get("network")
+    tokenAddress = request.args.get("tokenAddress")
+    return jsonify({
+        "status": 1,
+        "response": verify(network, tokenAddress)
+    })
