875bafcaeec4f21456ee8da0e03a941df3161b1a
Chugaister <dmytroshvahyliak@gmail.com>
2023-01-28 12:26:46 +0200

    routing added

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,5 @@
 from flask import Flask
+from os import path
 
+DIR = path.split(__file__)[0]
 app = Flask(__name__)
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,36 @@
-from flask import render_template
+from flask import render_template, send_file
 from app import app
+from os.path import join
+
+from . import DIR
 
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template("index.html")
+
+
+@app.route("/API")
+def api_docs():
+    return render_template("API.html")
+
+
+@app.route("/contact-us")
+def contact_us():
+    return render_template("contact-us.html")
+
+
+@app.route("/img/logoLOGO.png")
+def logo():
+    return send_file(join(DIR, "img", "logoLOGO.png"))
+
+
+@app.route("/img/IMGIMGG.png")
+def imgimg():
+    return send_file(join(DIR, "img", "IMGIMGG.png"))
+
+
+@app.route("/img/figuresBACKGROUND.jpg")
+def bg():
+    return send_file(join(DIR, "img", "figuresBACKGROUND.jpg"))
