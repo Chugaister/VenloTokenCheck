c6d4db0c9f448b727a789ee976f0a470795d6f58
zulumbaa <122629491+zulumbaa@users.noreply.github.com>
2023-01-28 20:57:57 +0200

    added result show

--- app/static/script.js
+++ app/static/script.js
@@ -1,25 +1,72 @@
-
-const sendForm = document.querySelector("#send-form");
-let URLaddress = window.location.protocol + "//" + window.location.host;
+let resultArticle = document.getElementsByClassName("result");
+const sendForm = document.querySelector('#send-form');
+let URLaddress = window.location.protocol + "//" + window.location.host + "/api/verify";
+let requestResult = new XMLHttpRequest();
+requestResult.open('GET', URLaddress );
+requestResult.responseType = 'json';
+requestResult.send();
+let counter = 0;
+if(counter==0){
 sendForm.onclick = function (event) {
-  const networkValue = document.querySelector(".network").value;
-  const addressValue = document.querySelector(".address").value;
-  URLaddress =
-    URLaddress + "?network=" + networkValue + "&tokenAddress=" + addressValue;
-  if (addressValue != "" && networkValue != "") {
-    event.preventDefault();
-     window.location.href = URLaddress;
-    /*window.location.href = "https://www.youtube.com/watch?v=JkLz-Ny_CFo";*/
+    const networkValue = document.querySelector('.network').value;
+    const addressValue = document.querySelector('.address').value;
+    URLaddress = URLaddress + "?network=" + networkValue + "&tokenAddress=" + addressValue;
+    if (addressValue != "" && networkValue != "") {
+        event.preventDefault();
+        let listOfParameters = requestResult.response;
+        let description = listOfParameters['response']['description'];
+        let mySection = document.getElementById("result")
+        let myArticle = document.createElement('article');
+        let info = document.createElement('p');
 
+        myArticle.appendChild(info);
+        info.textContent = description;
+        mySection.appendChild(myArticle);
+        let color = listOfParameters['response']['scam'];
+        if (color == true) {
+            info.setAttribute("style", "color: rgb(92, 208, 128)")
+        } else if (color == false) {
+            info.setAttribute("style", "color: rgb(224, 53, 53)")
+
+        } else {
+            info.setAttribute("style", "color: white")
+
+        }
+        counter++;
+
+
+    }
+}
+
+
+}
+
+
+/*XMLHttpRequest()*/
+
+   let requestURL = window.location.protocol + "//" + window.location.host + "/api/networks";
+   let request = new XMLHttpRequest();
+   request.open('GET', requestURL);
+   request.responseType = 'json';
+   request.send();
+   request.onload = function()
+  {
+   let listOfNetworks = request.response;
+   InputInDropbox(listOfNetworks);
+   console.log(listOfNetworks);
   }
-};
+  /*function InputInDropbox()*/
+function InputInDropbox(jsonObj)
+{
+  let networks = jsonObj['response'];
+  let mySelect = document.getElementById("select");
 
+  for(let i = 0; i < networks.length; i++){
+    let option = document.createElement('option');
+    option.type = 'hidden';
+    mySelect.appendChild(option);
+    option.textContent = networks[i];
+    option.setAttribute("value", String(networks[i]));
+  }
+}
 
-async function getUsers() {
-        let response = await fetch(
-          "http://127.0.0.1:5000/api/networks"
-        );
-        let data = await response.json();
-        return data;
-      }
-      getUsers().then(data => console.log(data));
